34f2d1f434a1d41e19a56deed1cb08449118050d
Hail Mary <41487770+alejosjen@users.noreply.github.com>
2018-10-23 18:05:16 -0700

    javascript update with jewel iterations and total counts toward target number

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,45 +1,33 @@
-// Start game
-//on click to target jewels
 
 var counter1 = 0;
 var counter2 = 0;
 var counter3 = 0;
 var counter4 = 0;
-var targetNumber = 45;
 
+var targetNumber = 55;
 $(".random-number-to-match").text(targetNumber);
 
-$(".j1").on("click", function () {
-    counter1 += 10;
+var numberOptions = [10, 5, 3, 7];
+
+$(".jewel").each(function(i, el) {
+    $(el).attr('data-crystalvalue', numberOptions[i])
+})
+
+$(".jewel").on("click", function () {
+    var crystalValue = ($(this).attr("data-crystalvalue"));
+    crystalValue = parseInt(crystalValue);
+
+    counter1 += crystalValue;
+
     alert("Your new score is: " + counter1);
-    /* if (counter1 === targetNumber) {
+
+    if (counter1 === targetNumber) {
         alert("You win!");
-    } else if (counter1 >= targetNumber) {
+    }
+
+    else if (counter1 >= targetNumber) {
         alert("You lose!");
-    } */
-//Trying to combine two functions to be able to add counters.    
-// This is acting weird. It's counting counter2 multiple times
-// after it's been counted already, then the user loses no
-// matter what.
-    $(".j2").on("click", function () {
-        counter2 += 5;
-        alert("Your new score is: " + counter2);
+    }
 
-        if (counter1 + counter2 === targetNumber) {
-            alert("You win!");
-
-        } else if (counter1 + counter2 >= targetNumber) {
-            alert("You lose!");
-        }
-    });
-
-});
-$(".j3").on("click", function () {
-    counter3++;
-    alert("Your new score is: " + counter3)
-});
-$(".j4").on("click", function () {
-    counter4++;
-    alert("Your new score is: " + counter4)
 });
 
