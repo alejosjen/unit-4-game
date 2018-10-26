ffd9471b2b5e3ae53878bcf3865d24024e6e975f
Hail Mary <41487770+alejosjen@users.noreply.github.com>
2018-10-25 21:58:19 -0700

    Javascript updated to set total score to zero when round is complete, wins and losses recorded, reset function developed

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,38 +1,63 @@
 
-var counter1 = 0;
-var counter2 = 0;
-var counter3 = 0;
-var counter4 = 0;
+var counter = 0;
+var targetNumber = 0;
+var wins = 0;
+var losses = 0;
 
-var targetNumber = 55;
-
-
-function getRandom (min, max) {
+// Get random number from 19-120 and display at .random-number-to-match
+function getRandom(min, max) {
     return Math.round(Math.random() * (max - min) + min);
 }
-targetNumber = getRandom(19, 120);
-$(".random-number-to-match").text(targetNumber);
 
-var numberOptions = [20, 5, 3, 7];
+function setTargetNumber() {
+    targetNumber = getRandom(19, 120);
+    $(".random-number-to-match").text(targetNumber);
+}
 
-$(".jewel").each(function(i, el) {
+//Reset
+function reset(){
+    counter = 0;
+    setTargetNumber();
+}
+
+function changeCurrentScore(score){
+    counter = score;
+}
+
+//Start game
+reset();
+
+// Create a value for each jewel from the numberOptions array
+var numberOptions = [12, 5, 3, 9];
+
+$(".jewel").each(function (i, el) {
     $(el).attr('data-crystalvalue', numberOptions[i])
-})
+});
 
+// On click function for each jewel to be assigned a value
+// Increase counter with each click
+// Alert you win if value of clicks equals the random number
+// Alert you lose if value goes over
 $(".jewel").on("click", function () {
     var crystalValue = ($(this).attr("data-crystalvalue"));
     crystalValue = parseInt(crystalValue);
 
-    counter1 += crystalValue;
+    counter += crystalValue;
 
-    alert("Your new score is: " + counter1);
-
-    if (counter1 === targetNumber) {
+    $(".score-total").text(counter);
+    if (counter === targetNumber) {
         alert("You win!");
+        wins++;
+        $(".wins-counter").text(wins);
+        reset();
+        $(".score-total").text(counter)
     }
-
-    else if (counter1 >= targetNumber) {
+    else if (counter >= targetNumber) {
         alert("You lose!");
+        losses++;
+        $(".losses-counter").text(losses);
+        reset();
+        $(".score-total").text(counter)
     }
 
 });
